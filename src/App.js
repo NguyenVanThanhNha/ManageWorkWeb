d9ec5cc3dadd5fb2979c7f617cc4803296b9f5e6
Thanh Nha Nguyen Van <nhathanh1022@gmail.com>
2022-03-11 00:18:40 +0700

    Design ManageWork FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="managework-thanhnhadev-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-column">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
+              alt='thanhnhadev-alt-img'/>
+              Title: THANHNHADEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
+              alt='thanhnhadev-alt-img'/>
+              Title: THANHNHADEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
+              alt='thanhnhadev-alt-img'/>
+              Title: THANHNHADEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
+              alt='thanhnhadev-alt-img'/>
+              Title: THANHNHADEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
+              alt='thanhnhadev-alt-img'/>
+              Title: THANHNHADEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
+              alt='thanhnhadev-alt-img'/>
+              Title: THANHNHADEV
+            </li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+            <li>Add what you'd like to work on below</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
