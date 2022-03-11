b1f38340aa6d11d09ffdbc97250a5475730e5cde
Thanh Nha Nguyen Van <nhathanh1022@gmail.com>
2022-03-11 14:02:07 +0700

    updata react architecture

--- src/App.js
+++ src/App.js
@@ -1,102 +1,17 @@
 import './App.scss';
+import React from 'react';
+
+//custom component
+import AppBar from 'components/AppBar/AppBar';
+import BoardBar from 'components/BoardBar/BoardBar';
+import BoardContent from 'components/BoardContent/BoardContent';
 
 function App() {
   return (
     <div className="managework-thanhnhadev-master">
-      <nav className="navbar app">App bar</nav>
-      <nav className="navbar board">Board bar</nav>
-      <div className="board-column">
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
-              alt='thanhnhadev-alt-img'/>
-              Title: THANHNHADEV
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
-              alt='thanhnhadev-alt-img'/>
-              Title: THANHNHADEV
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
-              alt='thanhnhadev-alt-img'/>
-              Title: THANHNHADEV
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
-              alt='thanhnhadev-alt-img'/>
-              Title: THANHNHADEV
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
-              alt='thanhnhadev-alt-img'/>
-              Title: THANHNHADEV
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-        <div className="column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
-              alt='thanhnhadev-alt-img'/>
-              Title: THANHNHADEV
-            </li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-            <li>Add what you'd like to work on below</li>
-          </ul>
-          <footer>Add another card</footer>
-        </div>
-      </div>
+      <AppBar/>
+      <BoardBar/>
+      <BoardContent/>
     </div>
   );
 }
---
+++ src/components/Task/Task.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+import './Task.scss'
+
+function Task() {
+    return (
+            <li className="task-item">
+              <img src="https://i0.wp.com/www.fliptan.com/wp-content/uploads/2022/03/shutterstock_2021164787.jpg?fit=2392%2C1342&ssl=1"
+              alt='thanhnhadev-alt-img'/>
+              Title: THANHNHADEV
+            </li>
+    )
+}
+
+export default Task    
